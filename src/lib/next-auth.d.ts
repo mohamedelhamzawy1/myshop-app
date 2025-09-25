8de616af276495274c7dd2c8ba3b233c0353ff0e
Ahmedelhamzawy <ahmedelhamzawy9@gmail.com>
2025-09-26 02:16:08 +0300

    Move next-auth.d.ts to lib folder for Vercel build fix

--- src/lib/next-auth.d.ts
+++ src/lib/next-auth.d.ts
@@ -2,9 +2,7 @@ import { USerResponse } from "@/interfaces/login"
 import NextAuth, { User } from "next-auth"
 
 declare module "next-auth" {
-  /**
-   * Returned by `useSession`, `getSession` and received as a prop on the `SessionProvider` React Context
-   */
+  
   interface Session {
       user: USerResponse,
 
