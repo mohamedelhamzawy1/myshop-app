ad22ff1ef32842374001b154df4908cf29cb1169
Ahmedelhamzawy <ahmedelhamzawy9@gmail.com>
2025-09-26 03:28:25 +0300

    fix: wrap LoginForm in Suspense to support useSearchParams

--- src/app/(pages)/login/page.tsx
+++ src/app/(pages)/login/page.tsx
@@ -1,12 +1,16 @@
-import React from 'react'
-import { LoginForm } from './_Components/LoginForm/LoginForm'
+"use client";
+
+import React, { Suspense } from "react";
+import { LoginForm } from "./_Components/LoginForm/LoginForm";
 
 export default function Login() {
   return (
-    <div className='min-h-[60vh] flex flex-col justify-center items-center gap-8'>
-      <h1 className='text-4xl font-bold'>Welcome Back !</h1>
-      <LoginForm/>
+    <div className="min-h-[60vh] flex flex-col justify-center items-center gap-8">
+      <h1 className="text-4xl font-bold">Welcome Back !</h1>
+      <Suspense fallback={<div>Loading login form...</div>}>
+        <LoginForm />
+      </Suspense>
     </div>
-    
-  )
+  );
 }
+
